971a67dc68230a02b44032918b905b40d4404ad3
Prateek Kumar <kumarprateek1833@gmail.com>
2025-02-23 10:03:24 +0530

    cloudConfig fixed

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,7 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const admin = require("firebase-admin");
-const serviceAccount = require("./utils/firebaseConfig.json"); // Path to your Firebase key
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -17,9 +16,21 @@ const storage = new CloudinaryStorage({
     },
 });
 
-
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix for multiline key
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 
